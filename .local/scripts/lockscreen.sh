b3891b72f3988a7484b497268174e864320fe8f7
Manuel Luna <manuellunada@gmail.com>
2021-09-11 15:41:53 -0500

    screen lock

---
+++ .local/scripts/lockscreen.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#00000000'
+DEFAULT='#282828'
+TEXT='#ebdbb2'
+WRONG='#cc241d'
+VERIFYING='#00000000'
+
+i3lock \
+-i $HOME/.cache/lockscreen.png \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$BLANK     \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+\
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--time-str="%I:%M %P"        \
+--verif-text="Hmmm..."       \
+--wrong-text="Wrong!"        \
+--no-modkey-text             \
+--time-size=50               \
+--radius=120
