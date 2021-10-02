8cbf5675d88c2fa254ba84a1eab60881ae84e49b
Manuel Luna <manuellunada@gmail.com>
2021-10-01 20:55:55 -0500

    net

---
+++ .local/scripts/network/get_list.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+show_info() {
+	echo -en "\0prompt\x1f\n"
+	nmcli connection show | tail -n +2 | awk '{ print $1 }'
+}
+
+network=$(awk '{ print $1 }' <<< "$1")
+[ -n "$1" ] && nmcli connection up "$network" 2>&1 >/dev/null || show_info
+
